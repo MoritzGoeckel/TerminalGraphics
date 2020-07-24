8ebc3954f350c835ad5a26b8cf2a1dc13684ba0f
Moritz Goeckel <moritz.goeckel@gmail.com>
2020-07-24 21:15:27 +0200

    Formatting

--- Point.h
+++ Point.h
@@ -1,22 +1,19 @@
 #pragma once
 
-#include<iostream>
+#include <iostream>
 
-class Point{
-    private:
-        using T = int;
-        T x;
-        T y;
+class Point {
+   private:
+    using T = int;
+    T x;
+    T y;
 
-    public:
-        Point(T x, T y) : x(x), y(y){ }
-        T getX() { return x; }
-        T getY() { return y; }
-        void print(){
-            std::cout << "("
-                      << std::to_string(x)
-                      << " "
-                      << std::to_string(y)
-                      << ")";
-        }
+   public:
+    Point(T x, T y) : x(x), y(y) {}
+    T getX() { return x; }
+    T getY() { return y; }
+    void print() {
+        std::cout << "(" << std::to_string(x) << " " << std::to_string(y)
+                  << ")";
+    }
 };
--- UpdateLoop.h
+++ UpdateLoop.h
@@ -1,38 +1,38 @@
 #pragma once
 
 #include <chrono>
-#include <thread>
 #include <functional>
+#include <thread>
 
-template<typename Fn>
-class UpdateLoop{
-    private:
+template <typename Fn>
+class UpdateLoop {
+   private:
     int framerate;
     bool isStop;
     Fn task;
 
-    void startLoop(){
+    void startLoop() {
         int desiredWait = 1000 / this->framerate;
-        while(!isStop){
+        while (!isStop) {
             auto start = std::chrono::steady_clock::now();
-            task();
+            task();  // Perform the task
             auto end = std::chrono::steady_clock::now();
-            int elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            int elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                              end - start)
+                              .count();
             int waiting = desiredWait - elapsed;
-            if(waiting > 0){
+            if (waiting > 0) {
                 std::this_thread::sleep_for(std::chrono::milliseconds(waiting));
             }
         }
     }
 
-    public:
-    UpdateLoop(int framerate, Fn& task) : task(task){
+   public:
+    UpdateLoop(int framerate, Fn& task) : task(task) {
         this->framerate = framerate;
         this->isStop = false;
         this->startLoop();
     }
 
-    void stop(){
-        this->isStop = true;
-    }
+    void stop() { this->isStop = true; }
 };
